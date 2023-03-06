19b15da38adcea39f0526dccb88bba3cbaa814f4
MrIsmayil <mrismayil2012@gmail.com>
2023-03-06 13:19:28 +0300

    Fixed spaces

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -98,8 +98,8 @@
                 </b-card-text>
               </div>
             </b-card>
-          </router-link>        
-        </div>          
+          </router-link>
+        </div>
         </b-col>
 
         <!-- no result found -->
@@ -199,7 +199,7 @@
               </div>
             </b-card>
           </router-link>
-        </div>          
+        </div>
         </b-col>
 
         <!-- no result found -->
